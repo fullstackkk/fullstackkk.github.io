c751fb8b0b661bfb8bb95e68360d13f223916897
Alex Smit01~ <AlexSmit17264@gmail.com>
2022-05-10 23:08:11 +0300

    add page in git, and hosting

---
+++ build/js/scripts.js
@@ -0,0 +1,108 @@
+// Custom Scripts
+// burgerMenu
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+        if (!menu.classList.contains('active')) {
+            menu.classList.add('active')
+            burger.classList.add('active-burger')
+            body.classList.add('locked')
+        } else {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 768.98) {
+            menu.classList.remove('active')
+            burger.classList.remove('active-burger')
+            body.classList.remove('locked')
+        }
+    })
+}
+burgerMenu()
+
+
+// tabs 
+
+function tabs(headerSelector, tabSelector, contentSelector, activeClass, display = 'flex') {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector)
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none'
+        });
+        tab.forEach(item => {
+            item.classList.remove(activeClass)
+        });
+    }
+    function showTabContent(i = 0) {
+        content[i].style.display = display
+        tab[i].classList.add(activeClass)
+    }
+    hideTabContent()
+    showTabContent()
+    header.addEventListener('click', e => {
+        const target = e.target
+        if (target.classList.contains(tabSelector.replace(/\./, '')) ||
+            target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent()
+                    showTabContent(i)
+                }
+            });
+        }
+    })
+}
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+tabs('.tabs__header', '.tabs__header-item', '.tabs__content-item', 'active')
+
+//Skroll
+function toSkroll() {
+    const links = document.querySelector('.menu')
+    const header = document.querySelector('header')
+    console.log(links)
+    links.addEventListener('click', e => {
+        const target = e.target;
+        switch (target.name) {
+            case 'home': window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+                break;
+            case 'about': window.scrollTo({
+                top: 605,
+                behavior: "smooth"
+            })
+                break;
+            case 'service': window.scrollTo({
+                top: 1255,
+                behavior: "smooth"
+            })
+                break;
+            case 'work': window.scrollTo({
+                top: 1655,
+                behavior: "smooth"
+            })
+                break;
+        }
+        console.log(target.name);
+    })
+}
+
+
+
+
+
+
+
+toSkroll()
